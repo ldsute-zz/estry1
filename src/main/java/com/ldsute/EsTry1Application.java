86e6f936eca4313cb3cab54dbf547cc2fbefe498
Justin Stewart <justin@jjsmbp2015.local>
2017-02-20 15:10:01 -0700

    Initial commit - from start.spring.io

---
+++ src/main/java/com/ldsute/EsTry1Application.java
@@ -0,0 +1,12 @@
+package com.ldsute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTry1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsTry1Application.class, args);
+	}
+}
